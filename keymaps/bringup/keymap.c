718cc2fd843edd84fdddd886b595d8e010d82429
SinaSalvatrice <sinagessner.wuppertal@gmail.com>
2026-03-17 13:02:50 +0100

    x

--- keymaps/bringup/keymap.c
+++ keymaps/bringup/keymap.c
@@ -1,4 +1,5 @@
 #include QMK_KEYBOARD_H
+#include "timer.h"
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
     (void)index;
@@ -18,6 +19,57 @@ void keyboard_post_init_user(void) {
     rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING + 3);
     rgblight_set_speed_noeeprom(128);
     rgblight_sethsv_noeeprom(170, 255, 80);
+}
+#endif
+
+#ifdef OLED_ENABLE
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+    return rotation;
+}
+
+static void render_welcome_animation(void) {
+    uint8_t frame = (timer_read32() / 500) % 4;
+
+    oled_clear();
+    switch (frame) {
+        case 0:
+            oled_set_cursor(3, 1);
+            oled_write_ln_P(PSTR("9KEY"), false);
+            oled_set_cursor(1, 3);
+            oled_write_ln_P(PSTR("MAKRO"), false);
+            oled_set_cursor(0, 5);
+            oled_write_ln_P(PSTR("MASTER"), false);
+            break;
+        case 1:
+            oled_set_cursor(1, 0);
+            oled_write_ln_P(PSTR("9key_makro"), false);
+            oled_set_cursor(2, 2);
+            oled_write_ln_P(PSTR("_master"), false);
+            oled_set_cursor(4, 5);
+            oled_write_ln_P(PSTR("hello"), false);
+            break;
+        case 2:
+            oled_set_cursor(0, 1);
+            oled_write_ln_P(PSTR("> 9key <"), false);
+            oled_set_cursor(1, 3);
+            oled_write_ln_P(PSTR("makro"), false);
+            oled_set_cursor(1, 5);
+            oled_write_ln_P(PSTR("master"), false);
+            break;
+        default:
+            oled_set_cursor(0, 1);
+            oled_write_ln_P(PSTR("WELCOME TO"), false);
+            oled_set_cursor(0, 3);
+            oled_write_ln_P(PSTR("9key_makro"), false);
+            oled_set_cursor(1, 5);
+            oled_write_ln_P(PSTR("master"), false);
+            break;
+    }
+}
+
+bool oled_task_user(void) {
+    render_welcome_animation();
+    return false;
 }
 #endif
 
