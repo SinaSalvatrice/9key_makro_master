c7f6a2040e053ace898e3c82bd78a790dd66d276
SinaSalvatrice <sinagessner.wuppertal@gmail.com>
2026-03-17 04:13:12 +0100

    Update config.h

--- config.h
+++ config.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #define I2C_DRIVER I2CD1
-#define I2C1_SDA_PIN GP1
-#define I2C1_SCL_PIN GP0
+#define I2C1_SDA_PIN GP0
+#define I2C1_SCL_PIN GP1
 #define OLED_DISPLAY_128X64
 #define OLED_TIMEOUT 60000
 
