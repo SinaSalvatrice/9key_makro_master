3191551828aeb90f5e0418e837df4208f47547ce
SinaSalvatrice <sinagessner.wuppertal@gmail.com>
2026-03-17 03:54:15 +0100

    Reapply "bringup plus encoder"
    This reverts commit 6cac7953dddd7ccafedc141e099280d3a105ef6f.

--- keymaps/bringup/keymap.c
+++ keymaps/bringup/keymap.c
@@ -1,5 +1,17 @@
 #include QMK_KEYBOARD_H
 
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    (void)index;
+
+    if (clockwise) {
+        tap_code(KC_VOLU);
+    } else {
+        tap_code(KC_VOLD);
+    }
+
+    return false;
+}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_1, KC_2, KC_3,
