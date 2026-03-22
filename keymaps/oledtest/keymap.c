d3f8293db15be45e519ead51959ac185baf9ceb6
SinaSalvatrice <sinagessner.wuppertal@gmail.com>
2026-03-22 14:28:33 +0100

    l

--- keymaps/oledtest/keymap.c
+++ keymaps/oledtest/keymap.c
@@ -1,15 +1,4 @@
 #include QMK_KEYBOARD_H
-#include "gpio.h"
-
-void keyboard_post_init_user(void) {
-    gpio_set_pin_output(OLED_POWER_PIN);
-    gpio_write_pin_high(OLED_POWER_PIN);
-    wait_ms(50);
-
-    gpio_set_pin_output(ONBOARD_LED_PIN);
-    gpio_write_pin_high(ONBOARD_LED_PIN);
-}
-
 #ifdef OLED_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     return rotation;
@@ -22,7 +11,6 @@ bool oled_task_user(void) {
     oled_write_ln_P(PSTR("9KEY MAKRO"), false);
     oled_write_ln_P(PSTR("MASTER"), false);
     oled_write_ln_P(PSTR("ADDR 0x3C"), false);
-    oled_write_ln_P(PSTR("PWR GP25"), false);
     oled_write_ln_P(PSTR("I2C GP0/GP1"), false);
     return false;
 }
