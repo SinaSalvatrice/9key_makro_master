ce1f5306221a8539c9179dbbae2e5ab9c0e9ff47
Sina <sinagessner.wuppertal@gmail.com>
2026-03-23 17:10:17 +0100

    Update keymap.c

--- keymaps/oledtest/keymap.c
+++ keymaps/oledtest/keymap.c
@@ -85,6 +85,63 @@ static void refresh_feedback(void) {
     apply_layer_rgb(visual_layer());
 }
 
+layer_state_t layer_state_set_user(layer_state_t state) {
+    if (!selector_active) {
+        apply_layer_rgb(get_highest_layer(state | default_layer_state));
+    }
+    return state;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        last_keycode = keycode;
+        last_row     = record->event.key.row;
+        last_col     = record->event.key.col;
+    }
+    return true;
+}
+
+#ifdef OLED_ENABLE
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+    return rotation;
+}
+
+static void render_selector(void) {
+    oled_write_ln_P(PSTR("BAS NAV EDT"), false);
+    oled_write_ln_P(PSTR("MED FN  RGB"), false);
+    oled_write_P(PSTR("SEL -> "), false);
+    oled_write_ln(layer_name(selector_target), false);
+    oled_write_P(PSTR("POS "), false);
+    oled_write(get_u8_str(last_row, ' '), false);
+    oled_write_P(PSTR(","), false);
+    oled_write_ln(get_u8_str(last_col, ' '), false);
+}
+
+bool oled_task_user(void) {
+    char keycode_hex[8];
+
+    oled_clear();
+    oled_write_P(PSTR("Layer "), false);
+    oled_write_ln(layer_name(visual_layer()), false);
+
+    if (selector_active) {
+        render_selector();
+        return false;
+    }
+
+    snprintf(keycode_hex, sizeof(keycode_hex), "0x%04X", last_keycode);
+
+    oled_write_P(PSTR("Key   "), false);
+    oled_write_ln(keycode_hex, false);
+    oled_write_P(PSTR("Pos   "), false);
+    oled_write(get_u8_str(last_row, ' '), false);
+    oled_write_P(PSTR(","), false);
+    oled_write_ln(get_u8_str(last_col, ' '), false);
+
+    return false;
+}
+#endif
+
 void keyboard_post_init_user(void) {
     gpio_set_pin_input_high(ENCODER_BTN_PIN);
 
@@ -93,13 +150,6 @@ void keyboard_post_init_user(void) {
     gpio_write_pin_high(GP25);
 
     refresh_feedback();
-}
-
-layer_state_t layer_state_set_user(layer_state_t state) {
-    if (!selector_active) {
-        apply_layer_rgb(get_highest_layer(state | default_layer_state));
-    }
-    return state;
 }
 
 static void begin_selector(void) {
@@ -151,57 +201,6 @@ void matrix_scan_user(void) {
 
     was_pressed = pressed;
 }
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (record->event.pressed) {
-        last_keycode = keycode;
-        last_row     = record->event.key.row;
-        last_col     = record->event.key.col;
-    }
-
-    return true;
-}
-
-#ifdef OLED_ENABLE
-oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    return rotation;
-}
-
-static void render_selector(void) {
-    oled_write_ln_P(PSTR("BAS NAV EDT"), false);
-    oled_write_ln_P(PSTR("MED FN  RGB"), false);
-    oled_write_P(PSTR("SEL -> "), false);
-    oled_write_ln(layer_name(selector_target), false);
-    oled_write_P(PSTR("POS "), false);
-    oled_write(get_u8_str(last_row, ' '), false);
-    oled_write_P(PSTR(","), false);
-    oled_write_ln(get_u8_str(last_col, ' '), false);
-}
-
-bool oled_task_user(void) {
-    char keycode_hex[8];
-
-    oled_clear();
-    oled_write_P(PSTR("Layer "), false);
-    oled_write_ln(layer_name(visual_layer()), false);
-
-    if (selector_active) {
-        render_selector();
-        return false;
-    }
-
-    snprintf(keycode_hex, sizeof(keycode_hex), "0x%04X", last_keycode);
-
-    oled_write_P(PSTR("Key   "), false);
-    oled_write_ln(keycode_hex, false);
-    oled_write_P(PSTR("Pos   "), false);
-    oled_write(get_u8_str(last_row, ' '), false);
-    oled_write_P(PSTR(","), false);
-    oled_write_ln(get_u8_str(last_col, ' '), false);
-
-    return false;
-}
-#endif
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
     (void)index;
@@ -271,15 +270,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_RGB] = LAYOUT(
-        UG_SPDU, UG_SPDD, UG_TOGG,
-        UG_HUEU, UG_HUED, UG_VALU,
-        UG_SATU, UG_SATD, UG_VALD
+        UG_TOGG, UG_HUEU, UG_HUED,
+        UG_SATU, UG_VALU, UG_VALD,
+        UG_SATD, UG_NEXT, UG_PREV
     ),
 
     [_SELECT] = LAYOUT(
         TO(_NAV),  TO(_EDIT), TO(_MEDIA),
         TO(_FN),   KC_TRNS,   TO(_RGB),
         KC_NO,     KC_NO,     KC_NO
-    ),
+    )
 };
-    
