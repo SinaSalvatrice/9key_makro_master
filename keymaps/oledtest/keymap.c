efe7767a3cb2c5e7bb9c6547ec6715deb185d3ae
SinaSalvatrice <sinagessner.wuppertal@gmail.com>
2026-03-23 01:45:44 +0100

    Update keymap.c

--- keymaps/oledtest/keymap.c
+++ keymaps/oledtest/keymap.c
@@ -2,6 +2,12 @@
 
 enum layers {
     _BASE,
+    _NAV,
+    _EDIT,
+    _MEDIA,
+    _FN,
+    _RGB,
+    _SELECT,
 };
 
 static uint16_t last_keycode = KC_NO;
@@ -53,9 +59,45 @@ bool oled_task_user(void) {
 #endif
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-   [_BASE] = LAYOUT(
-    KC_1, KC_2, KC_3,
-    KC_4, KC_5, KC_6,
-    KC_7, KC_8, KC_9
-)
+    [_BASE] = LAYOUT(
+        KC_1, KC_2, KC_3,
+        KC_4, KC_5, KC_6,
+        KC_7, KC_8, KC_9
+    ),
+
+    [_NAV] = LAYOUT(
+        KC_LEFT, KC_UP,   KC_RGHT,
+        KC_HOME, KC_ENT,  KC_END,
+        KC_PGDN, KC_DOWN, KC_PGUP
+    ),
+
+    [_EDIT] = LAYOUT(
+        LCTL(KC_Z), LCTL(KC_C), LCTL(KC_V),
+        LCTL(KC_X), KC_ENT,     KC_BSPC,
+        LCTL(KC_A), KC_SPC,     LCTL(LSFT(KC_Z))
+    ),
+
+    [_MEDIA] = LAYOUT(
+        KC_MPRV, KC_MPLY, KC_MNXT,
+        KC_MUTE, KC_MSTP, KC_VOLU,
+        KC_NO,   KC_NO,   KC_VOLD
+    ),
+
+    [_FN] = LAYOUT(
+        KC_F13, KC_F14, KC_F15,
+        KC_F16, KC_F17, KC_F18,
+        KC_F19, KC_F20, KC_F21
+    ),
+
+    [_RGB] = LAYOUT(
+        KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO
+    ),
+
+    [_SELECT] = LAYOUT(
+        TO(_NAV),  TO(_EDIT),  TO(_MEDIA),
+        TO(_FN),   TO(_BASE),  TO(_RGB),
+        KC_NO,     KC_NO,      KC_NO
+    )
 };
