f869b4638f17d458ad016e9a623add8c9ddb785b
SinaSalvatrice <sinagessner.wuppertal@gmail.com>
2026-03-28 22:40:44 +0100

    a

--- keymaps/bringup/keymap.c
+++ keymaps/bringup/keymap.c
@@ -1,5 +1,4 @@
 #include QMK_KEYBOARD_H
-#include "timer.h"
 #include "gpio.h"
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
@@ -14,62 +13,40 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     return false;
 }
 
-#ifdef RGBLIGHT_ENABLE
 void keyboard_post_init_user(void) {
+#ifdef RGBLIGHT_ENABLE
     rgblight_enable_noeeprom();
     rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING + 3);
     rgblight_set_speed_noeeprom(128);
     rgblight_sethsv_noeeprom(170, 255, 80);
-}
 #endif
+
+    // Power on OLED via GP25
+    gpio_set_pin_output(GP25);
+    gpio_write_pin_high(GP25);
+}
 
 #ifdef OLED_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     return rotation;
 }
 
-static void render_welcome_animation(void) {
-    uint8_t frame = (timer_read32() / 500) % 4;
-
-    oled_clear();
-    switch (frame) {
-        case 0:
-            oled_set_cursor(3, 1);
-            oled_write_ln_P(PSTR("9KEY"), false);
-            oled_set_cursor(1, 3);
-            oled_write_ln_P(PSTR("MAKRO"), false);
-            oled_set_cursor(0, 5);
-            oled_write_ln_P(PSTR("MASTER"), false);
-            break;
-        case 1:
-            oled_set_cursor(1, 0);
-            oled_write_ln_P(PSTR("9key_makro"), false);
-            oled_set_cursor(2, 2);
-            oled_write_ln_P(PSTR("_master"), false);
-            oled_set_cursor(4, 5);
-            oled_write_ln_P(PSTR("hello"), false);
-            break;
-        case 2:
-            oled_set_cursor(0, 1);
-            oled_write_ln_P(PSTR("> 9key <"), false);
-            oled_set_cursor(1, 3);
-            oled_write_ln_P(PSTR("makro"), false);
-            oled_set_cursor(1, 5);
-            oled_write_ln_P(PSTR("master"), false);
-            break;
-        default:
-            oled_set_cursor(0, 1);
-            oled_write_ln_P(PSTR("WELCOME TO"), false);
-            oled_set_cursor(0, 3);
-            oled_write_ln_P(PSTR("9key_makro"), false);
-            oled_set_cursor(1, 5);
-            oled_write_ln_P(PSTR("master"), false);
-            break;
-    }
-}
+static bool oled_initialized = false;
 
 bool oled_task_user(void) {
-    render_welcome_animation();
+    if (oled_initialized) {
+        return false;
+    }
+    oled_initialized = true;
+
+    oled_clear();
+    oled_set_cursor(2, 1);
+    oled_write_ln_P(PSTR("9KEY"), false);
+    oled_set_cursor(1, 3);
+    oled_write_ln_P(PSTR("MAKRO"), false);
+    oled_set_cursor(0, 5);
+    oled_write_ln_P(PSTR("MASTER"), false);
+
     return false;
 }
 #endif
