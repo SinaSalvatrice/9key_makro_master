feda8d7760958f667c4f9ff242a997f55accc887
SinaSalvatrice <sinagessner.wuppertal@gmail.com>
2026-03-29 15:55:58 +0200

    Update keymap.c

--- keymaps/bringup/keymap.c
+++ keymaps/bringup/keymap.c
@@ -57,60 +57,66 @@ enum layers {
     _EDIT,
     _MEDIA,
     _FN,
-    _MAKRO,
-    _MAKRO2,
+    _DEV,
+    _DEV2,
     _RGB,
     _SELECT
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
     [_BASE] = LAYOUT(
-        MO(_SELECT),            KC_UP,              LALT(KC_TAB),
-        KC_LEFT,                LGUI(KC_TAB),       KC_RGHT,
+        KEY_TRNS,               KC_UP,              LALT(KC_TAB),
+        KC_LEFT,                KC_LEFT_GUI,        KC_RGHT,
         LCTL(KC_Z),             KC_DOWN,            LCTL(KC_R)
     ),
+
     [_NAV] = LAYOUT(
-        MO(_SELECT),            KC_UP,              LALT(KC_TAB),
+        KEY_TRNS,               KC_UP,              LALT(KC_TAB),
         KC_LEFT,                KC_ENT,             KC_RGHT,
         LCTL(KC_Z),             KC_DOWN,            LCTL(KC_R)
     ),
+
     [_EDIT] = LAYOUT(
-        MO(_SELECT),            LCTL(KC_C),         LCTL(KC_V),
+        KEY_TRNS,               LCTL(KC_C),         LCTL(KC_V),
         LCTL(KC_X),             LCTL(KC_ENT),       KC_NO,
         LCTL(LSFT(KC_Z)),       KC_SPC,             KC_BSPC
     ),
+
     [_MEDIA] = LAYOUT(
-        MO(_SELECT),            KC_MSEL,            KC_MNXT,
+        KEY_TRNS,               KC_MSEL,            KC_MNXT,
         KC_MRWD,                KC_MPLY,            KC_MFFD,
         KC_DOWN,                KC_MSTP,            KC_UP
     ),
+
+   [_DEV] = LAYOUT(
+        KEY_TRNS,               KC_NO,          TO(_MEDIA),
+        TO(_FN),                TO(_BASE),          KC_NO,
+        KC_NO,                  KC_NO,              KC_NO
+    ),
+
+    [_DEV2] = LAYOUT(
+        KEY_TRNS,               TO(_EDIT),          TO(_MEDIA),
+        TO(_FN),                TO(_BASE),          KC_NO,
+        KC_NO,                  KC_NO,              KC_NO
+    ),
+
     [_FN] = LAYOUT(
-        MO(_SELECT),            KC_F14,            KC_F15,
+        KEY_TRNS,               KC_F14,            KC_F15,
         KC_F16,                 KC_F17,            KC_F18,
         KC_F19,                 KC_F20,            KC_F21
     ),
+
     [_RGB] = LAYOUT(
         MO(_SELECT),            UG_SPDD,            UG_TOGG,
         UG_HUEU,                UG_HUED,            UG_VALU,
         UG_SATU,                UG_SATD,            UG_VALD
     ),
 
-   [_MAKRO] = LAYOUT(
-        KEY_TRNS,               TO(_EDIT),          TO(_MEDIA),
-        TO(_FN),                TO(_BASE),          KC_NO,
-        KC_NO,                  KC_NO,              KC_NO
-    ),
-
-    [_MAKRO2] = LAYOUT(
-        KEY_TRNS,               TO(_EDIT),          TO(_MEDIA),
-        TO(_FN),                TO(_BASE),          KC_NO,
-        KC_NO,                  KC_NO,              KC_NO
-    ),
-
-   [_SELECT] = LAYOUT(
-        KEY_TRNS,               TO(_EDIT),          TO(_MEDIA),
-        TO(_FN),                TO(_BASE),          KC_NO,
-        KC_NO,                  KC_NO,              KC_NO
+    [_SELECT] = LAYOUT(
+        KC_NO,                 TO(_BASE),           TO(_NAV),
+        TO(_EDIT),             TO(_MEDIA),          TO(_RGB),
+        TO(_DEV),               TO(_DEV2),            TO(_FN)
     ),
 };
 
